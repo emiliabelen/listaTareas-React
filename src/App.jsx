59a19260f51fe087cb5fadbbce9124379e05bbb0
belen ramos <ramos.emiliabelen@gmail.com>
2023-06-10 16:44:40 -0300

    agregamos el maquetado de listaTareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FormularioTareas from "./components/FormularioTareas";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <FormularioTareas
-        primerTitulo="Lista de Tareas"
-        estado={true}
-      ></FormularioTareas>
+<Container className = "my-5 mainPage">
+  <h1 className="display-4 text-center text-kight">Lista de Tareas</h1>
+  <hr className="text-ligth"/>
+  <FormularioTareas ></FormularioTareas>
+</Container>
+      
+      
 
       <footer className="bg-dark text-light text-center py-4">
         <p>&copy; Todos los derechos reservados</p>
--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,12 +1,8 @@
-const FormularioTareas = ({primerTitulo} ) => {
-    // console .log (props.FormularioTareas)
-    return (
-        <section>
-            <h1>Lista de Tareas</h1>
-            <hr />
-            <h2>{primerTitulo}</h2>
-        </section>
-    );
+const FormularioTareas = ({  }) => {
+  // console .log (props.FormularioTareas)
+  return (
+    <div></div>
+  );
 };
 
 export default FormularioTareas;
